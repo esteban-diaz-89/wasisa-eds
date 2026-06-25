623c2f48d7e2335a72a1f4ec2bd119f94ed6b327
esteban.diaz <esteban.diaz@ext.softtek.com>
2026-06-25 12:19:20 +0200

    added classlist in footer to tests where it appears

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -14,6 +14,7 @@ export default async function decorate(block) {
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
+  footer.classList.add('esteban-test');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
