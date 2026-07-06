612150bbeb6cf4374773b0bd7fa520a4e3975833
esteban.diaz <esteban.diaz@ext.softtek.com>
2026-07-06 08:42:29 +0200

    feat: div correctly selected now

--- blocks/medical-picture-searcher/medical-picture-searcher.js
+++ blocks/medical-picture-searcher/medical-picture-searcher.js
@@ -2,7 +2,7 @@ export default async function decorate(block) {
 
     const searchComponent = document.createElement('div');
     searchComponent.setAttribute('data-component', 'cmp-medical-picture-search-react');
-    block.querySelector('div > div').appendChild(searchComponent);
+    block.querySelector(':scope > div > div').appendChild(searchComponent);
 
     const event = new CustomEvent('LoadSearch', {
       detail: { }
