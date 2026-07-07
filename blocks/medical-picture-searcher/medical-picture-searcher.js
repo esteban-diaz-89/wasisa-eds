038ec76c01ee8b3132f6078a07dbdd1d44f9e7ae
esteban.diaz <esteban.diaz@ext.softtek.com>
2026-07-07 09:05:27 +0200

    changed result by results

--- blocks/medical-picture-searcher/medical-picture-searcher.js
+++ blocks/medical-picture-searcher/medical-picture-searcher.js
@@ -7,7 +7,7 @@ export default async function decorate(block) {
     searchComponent.setAttribute('data-component', 'cmp-medical-picture-search-react');
     const dataPropsJson = `{
         "data" : {
-            "view" : "result",
+            "view" : "results",
             "networkId" : "1",
             "networkName" : "Salud"
         },
